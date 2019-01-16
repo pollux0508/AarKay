586e7914c2af795775812d4d21f4d49efc6a64fb
Rahul Katariya <rahulkatariya@me.com>
2019-01-16 17:52:51 +0530

    Add ArgModel Spec

---
+++ Tests/AarKayPluginTests/Core/ArgModelSpec.swift
@@ -0,0 +1,153 @@
+//
+//  ArgModelSpec.swift
+//  AarKayPluginTests
+//
+//  Created by RahulKatariya on 16/01/19.
+//
+
+import Foundation
+import Nimble
+import Quick
+import Yams
+@testable import AarKayPlugin
+
+class ArgModelSpec: QuickSpec {
+    override func spec() {
+        describe("ArgModel") {
+            it("should work with simple type") {
+                expect { () -> Void in
+                    let argString = "name|String"
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "String"
+                    expect(argModel.swiftType) == "String"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == false
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == false
+                    expect(argModel.isOptionalOrWrapped) == false
+                }.toNot(throwError())
+            }
+            
+            it("should work with optional type") {
+                expect { () -> Void in
+                    let argString = "name|String?"
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "String?"
+                    expect(argModel.swiftType) == "String"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == false
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == true
+                    expect(argModel.isOptionalOrWrapped) == true
+                }.toNot(throwError())
+            }
+            
+            it("should work with default value") {
+                expect { () -> Void in
+                    let argString = "name|String!|Rahul"
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "String!"
+                    expect(argModel.swiftType) == "String"
+                    expect(argModel.value) == "Rahul"
+                    expect(argModel.isArray) == false
+                    expect(argModel.isWrapped) == true
+                    expect(argModel.isOptional) == false
+                    expect(argModel.isOptionalOrWrapped) == true
+                }.toNot(throwError())
+            }
+            
+            it("should work with array type") {
+                expect { () -> Void in
+                    let argString = "name|[String]"
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[String]"
+                    expect(argModel.swiftType) == "[String]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == true
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == false
+                    expect(argModel.isOptionalOrWrapped) == false
+                }.toNot(throwError())
+            }
+            
+            it("should work with optional array type") {
+                expect { () -> Void in
+                    let argString = "name|[String]?"
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[String]?"
+                    expect(argModel.swiftType) == "[String]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == true
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == true
+                    expect(argModel.isOptionalOrWrapped) == true
+                }.toNot(throwError())
+            }
+            
+            it("should work with optional dictionary type") {
+                expect { () -> Void in
+                    let argString = "\"name|[String: String]\""
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[String: String]"
+                    expect(argModel.swiftType) == "[String: String]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == false
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == false
+                    expect(argModel.isOptionalOrWrapped) == false
+                }.toNot(throwError())
+            }
+            
+            it("should work with optional dictionary type") {
+                expect { () -> Void in
+                    let argString = "\"name|[String: String]?\""
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[String: String]?"
+                    expect(argModel.swiftType) == "[String: String]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == false
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == true
+                    expect(argModel.isOptionalOrWrapped) == true
+                }.toNot(throwError())
+            }
+            
+            it("should work with array dictionary type") {
+                expect { () -> Void in
+                    let argString = "\"name|[[String: String]]\""
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[[String: String]]"
+                    expect(argModel.swiftType) == "[[String: String]]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == true
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == false
+                    expect(argModel.isOptionalOrWrapped) == false
+                }.toNot(throwError())
+            }
+            
+            it("should work with 2d array type") {
+                expect { () -> Void in
+                    let argString = "\"name|[[String]]?\""
+                    let argModel = try YAMLDecoder().decode(ArgModel.self, from: argString)
+                    expect(argModel.name) == "name"
+                    expect(argModel.type) == "[[String]]?"
+                    expect(argModel.swiftType) == "[[String]]"
+                    expect(argModel.value).to(beNil())
+                    expect(argModel.isArray) == true
+                    expect(argModel.isWrapped) == false
+                    expect(argModel.isOptional) == true
+                    expect(argModel.isOptionalOrWrapped) == true
+                    }.toNot(throwError())
+            }
+        }
+    }
+}
---
+++ Tests/AarKayPluginTests/Core/TemplateModelSpec.swift
@@ -0,0 +1,40 @@
+//
+//  TemplateModelSpec.swift
+//  AarKayPluginTests
+//
+//  Created by RahulKatariya on 16/01/19.
+//
+
+import Foundation
+import Nimble
+import Quick
+import Yams
+@testable import AarKayPlugin
+
+class TemplateModelSpec: QuickSpec {
+    override func spec() {
+        describe("TemplateModel") {
+            it("should work") {
+                expect { () -> Void in
+                    let templateString = """
+                    name: Template
+                    properties:
+                      - name|String
+                      - type|String?
+                      - version|Int!|1
+                    computedProperties:
+                      - arg|String?
+                    """
+                    let templateModel = try YAMLDecoder().decode(TemplateModel.self, from: templateString)
+                    expect(templateModel.properties.count) == 3
+                    expect(templateModel.requiredProperties?.count) == 1
+                    expect(templateModel.allProperties?.count) == 4
+                    expect(templateModel.requiredAllProperties?.count) == 1
+                    expect(templateModel.baseProperties?.count) == 0
+                    expect(templateModel.requiredBaseProperties?.count) == 0
+                    expect(templateModel.computedProperties?.count) == 1
+                }.toNot(throwError())
+            }
+        }
+    }
+}
