80540dc5aa723cb9d425fcfa3d771814d32d7921
Rahul Katariya <rahulkatariya@me.com>
2019-01-26 17:16:51 +0530

    Update SwiftyTextTable

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         /* >>> AarKay ------------------------------------------- */
         /* ------------------------------------------------------ */
         .package(url: "https://github.com/jdhealy/PrettyColors.git", .exact("5.0.1")),
-        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", .exact("0.8.0")),
+        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", .upToNextMinor(from: "0.8.2")),
         /* ------------------------------------------------------ */
         /* >>> Runner ------------------------------------------- */
         /* ------------------------------------------------------ */
