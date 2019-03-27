3152ad7eb24e9a49e9b6e507c71f465c432e9c37
Rahul Katariya <rahulkatariya@me.com>
2019-03-27 13:27:03 +0530

    Update Plugin Template

--- aarkay-plugin-pluginlowername/Package.swift
+++ aarkay-plugin-pluginlowername/Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AarKaypluginnamePlugin",
+    platforms: [
+        .macOS(.v10_12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
@@ -34,5 +37,5 @@ let package = Package(
             path: "Sources/AarKaypluginnamePlugin"
         ),
     ],
-    swiftLanguageVersions: [.v4, .v4_2]
+    swiftLanguageVersions: [.v5]
 )
--- aarkay-plugin-{{ cookiecutter.name|lower }}/Package.swift
+++ aarkay-plugin-{{ cookiecutter.name|lower }}/Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AarKay{{ cookiecutter.name }}Plugin",
+    platforms: [
+        .macOS(.v10_12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
@@ -34,5 +37,5 @@ let package = Package(
             path: "Sources/AarKay{{ cookiecutter.name }}Plugin"
         ),
     ],
-    swiftLanguageVersions: [.v4, .v4_2]
+    swiftLanguageVersions: [.v5]
 )
